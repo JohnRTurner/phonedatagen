a5150ee2f8c1546b02ccf31c73fb9aad292e90d9
rreddy <rohitreddyjd@gmail.com>
2022-10-17 13:37:22 -0600

    Added

--- datagenerators4.py
+++ datagenerators4.py
@@ -9,22 +9,26 @@ encoder.FLOAT_REPR = lambda o: format(o, '.2f')
 
 
 def gen_event(thread, fake, inv, linenumber):
+ ad_accnt_id = 8323779991
  rndcnt = fake.random_number( digits=8)
- shipdate = datetime.today() + timedelta(days = (rndcnt % 23) - 30)
- recdate = shipdate + timedelta(days =((rndcnt%4) + 1))
- comdate = shipdate + timedelta(days =((rndcnt%5) + 3))
- pkey = (rndcnt % 20000000) + 1
- skey = (rndcnt % 999999) + 1
+ ad_utc_date = datetime.today() + timedelta(days = (ad_accnt_id % 23) - 30)
+ recdate = shipdate + timedelta(days =((ad_accnt_id%4) + 1))
+ comdate = shipdate + timedelta(days =((ad_accnt_id%5) + 3))
+ pkey = (ad_accnt_id % 20000000) + 1
+ skey = (ad_accnt_id % 999999) + 1
  return {
-         "l_comment":fake.sentence(nb_words=3),
-         "l_commitdate":comdate.strftime('%Y-%m-%d'),
-         "l_discount":float(fake.pydecimal(left_digits = 2, right_digits = 2, positive = True, max_value = 25.0)),
-         "l_extendedprice":float(fake.pydecimal(left_digits = 5,right_digits = 2, positive = True, min_value = 1000, max_value = 99999)),
-         "l_linenumber":linenumber,
-         "l_linestatus":fake.pystr(min_chars=1, max_chars=1),
-         "l_orderkey": inv,
-         "l_partkey":pkey,
-         "l_quantity":fake.random_int(min=1, max=1000),
+         "AD_ACCOUNT_ID":ad_accnt_id,
+         "CAMPAIGN_ID":fake.random_int(min=8323779991100, max=8323779991200),
+         "AD_GROUP_ID":fake.random_int(min=8323779991100300, max=8323779991200600),
+         "AD_ID":fake.random_number( digits=8),
+         "KEYWORD_ID":fake.random_number( digits=13),
+         "CREATIVE_ID":fake.random_number( digits=18),
+         "MATCH_TYPE":fake.pystr(min_chars=10, max_chars=20),
+         "SEARCH_TERM": fake.pystr(min_chars=8, max_chars=24),
+         "SPEND":float(fake.pydecimal(left_digits = 6,right_digits = 4, positive = True, min_value = 1000, max_value = 999999)),
+         "IMPRESSIONS":fake.random_int(min=1, max=1000000),
+         "TAPS":fake.random_int(min=1, max=100000),
+         "DOWNLOAD":fake.random_int(min=1, max=10000),
          "l_receiptdate":recdate.strftime('%Y-%m-%d'),
          "l_returnflag":fake.pystr(min_chars=1, max_chars=1),
          "l_shipdate":shipdate.strftime('%Y-%m-%d'),
